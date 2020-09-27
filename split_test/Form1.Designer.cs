ef8c1448461cd191cb3f6b2ea3dff47e87a8640d
YunyunD <56820198+YunyunD@users.noreply.github.com>
2020-09-27 11:19:21 +0800

    Add files via upload

---
+++ split_test/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace split_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "读取数据，显示并绘图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 187);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(286, 237);
+            this.textBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(343, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(473, 232);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(343, 250);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(473, 232);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart1";
+            this.chart2.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 508);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ split_test/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace split_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("data_test.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            string str = sr.ReadToEnd();
+            Console.WriteLine(str+"\r\n");
+            textBox1.Text = str;
+            sr.Close();
+            //fileStream部分将文本内容全部读出到 str中
+
+            string[] H_data = str.Split(new char[] { '\n', '\r' });//按行分割 后，为什么hdata的length 是行数两倍+1？ 且在器中奇数位没任何数据
+
+            Console.WriteLine("H_data 0 is " + H_data[0] + "num0\r\n");
+            Console.WriteLine("H_data 1 is " + H_data[1] + "num1\r\n");// 为什么hdata1中没任何数据？
+
+            
+            Console.WriteLine("str.length is " + str.Length + "\r\n");
+            string[] data = new string[str.Length];
+            //显示整体文本str.length
+            
+
+            /*
+            string str1 = "2.36917E-05";
+            float num1 = Convert.ToSingle(str1); //此段表明科学计数法可以直接通过convert来转成float
+            Console.WriteLine(str1 + "is {0} \r\n", num1);
+            */
+
+            Console.WriteLine("H_data.length is " + H_data.Length);//H_data.Length 为数据行数
+            int dataLength = H_data.Length / 2 + 1;
+            float[] x = new float[dataLength];
+            float[] y1 = new float[dataLength];
+            float[] y2 = new float[dataLength];
+
+            for (int i = 0; i < H_data.Length; i += 2)
+            {
+                // data = H_data[i].Split(new char[] { ' ' }); // 以空格为依据分割
+                data = H_data[i].Split('\t');//根据TAB 进行分割 
+
+                /* 检测data数据
+                Console.WriteLine("data {0} is " + data[0] + "\r\n", i);
+                Console.WriteLine("data {0} is " + data[1] + "\r\n", i);
+                Console.WriteLine("data {0} is " + data[2] + "\r\n", i);
+                Console.WriteLine("data length is " + data.Length + "\r\n");
+                */
+                
+                for (int j = 0; j < data.Length; j++) {
+                    switch (j) {
+                        case 0:
+                            x[i/2] = Convert.ToSingle(data[j]);
+                            break;
+                        case 1:
+                            y1[i/2] = Convert.ToSingle(data[j]);
+                            break;
+                        case 2:
+                            y2[i/2] = Convert.ToSingle(data[j]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+            }
+            /*
+            Console.WriteLine("x = {0} " + "\r\n", x[22]);
+            Console.WriteLine("y1 = {0} " + "\r\n", y1[22]);
+            Console.WriteLine("y2 = {0} " + "\r\n", y2[22]);
+            */
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            
+            for (int i = 0; i < dataLength; i++) {
+                chart1.Series[0].Points.AddXY(x[i], y1[i]);
+                chart2.Series[1].Points.AddXY(x[i], y2[i]);
+                chart1.Series[0].Color = Color.Red;
+                chart2.Series[1].Color = Color.Blue;
+                chart1.Series[0].BorderWidth = 1;
+                chart2.Series[1].BorderWidth = 1;
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+    }
+}
