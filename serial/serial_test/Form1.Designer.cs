bc720ac3c983763d16b5bb6b5df3fb095cd9d2ff
YunyunD <56820198+YunyunD@users.noreply.github.com>
2020-09-27 11:14:24 +0800

    Add files via upload

---
+++ serial/serial_test/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace serial_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.OpenPort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ClostPort = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SendContent = new System.Windows.Forms.Button();
+            this.buttonClearText = new System.Windows.Forms.Button();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.buttonMeasure = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenPort
+            // 
+            this.OpenPort.Location = new System.Drawing.Point(67, 44);
+            this.OpenPort.Name = "OpenPort";
+            this.OpenPort.Size = new System.Drawing.Size(75, 23);
+            this.OpenPort.TabIndex = 0;
+            this.OpenPort.Text = "打开串口";
+            this.OpenPort.UseVisualStyleBackColor = true;
+            this.OpenPort.Click += new System.EventHandler(this.OpenPort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "发送内容";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "接收内容";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(100, 131);
+            this.textBoxSend.Multiline = true;
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(125, 88);
+            this.textBoxSend.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 254);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 91);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // ClostPort
+            // 
+            this.ClostPort.Location = new System.Drawing.Point(169, 44);
+            this.ClostPort.Name = "ClostPort";
+            this.ClostPort.Size = new System.Drawing.Size(75, 23);
+            this.ClostPort.TabIndex = 5;
+            this.ClostPort.Text = "关闭串口";
+            this.ClostPort.UseVisualStyleBackColor = true;
+            this.ClostPort.Click += new System.EventHandler(this.ClostPort_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(100, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "串口号";
+            // 
+            // SendContent
+            // 
+            this.SendContent.Location = new System.Drawing.Point(273, 159);
+            this.SendContent.Name = "SendContent";
+            this.SendContent.Size = new System.Drawing.Size(75, 23);
+            this.SendContent.TabIndex = 9;
+            this.SendContent.Text = "发送";
+            this.SendContent.UseVisualStyleBackColor = true;
+            this.SendContent.Click += new System.EventHandler(this.SendContent_Click);
+            // 
+            // buttonClearText
+            // 
+            this.buttonClearText.Location = new System.Drawing.Point(273, 226);
+            this.buttonClearText.Name = "buttonClearText";
+            this.buttonClearText.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearText.TabIndex = 10;
+            this.buttonClearText.Text = "清空并初始化";
+            this.buttonClearText.UseVisualStyleBackColor = true;
+            this.buttonClearText.Click += new System.EventHandler(this.buttonClearText_Click);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(259, 268);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(125, 100);
+            this.textBoxInfo.TabIndex = 11;
+            // 
+            // buttonMeasure
+            // 
+            this.buttonMeasure.Location = new System.Drawing.Point(273, 44);
+            this.buttonMeasure.Name = "buttonMeasure";
+            this.buttonMeasure.Size = new System.Drawing.Size(75, 23);
+            this.buttonMeasure.TabIndex = 12;
+            this.buttonMeasure.Text = "实时测量绘图";
+            this.buttonMeasure.UseVisualStyleBackColor = true;
+            this.buttonMeasure.Click += new System.EventHandler(this.buttonMeasure_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(273, 86);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 13;
+            this.buttonStop.Text = "结束测量";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(403, 24);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(861, 498);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1286, 693);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonMeasure);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.buttonClearText);
+            this.Controls.Add(this.SendContent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ClostPort);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OpenPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button ClostPort;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SendContent;
+        private System.Windows.Forms.Button buttonClearText;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Button buttonMeasure;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ serial/serial_test/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace serial_test
+{
+
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort1 = new SerialPort();//定义新的串口类
+        private Queue<float> dataQueue = new Queue<float>(50); //新建一个五十位的队列，来堆栈测微机的数据
+        private int numDelete = 1; //每次删除增加几个点
+        private float receData ; //存放串口接受且经处理的数据，表示实际测量值
+        byte[] measureDataByte = new Byte[] { 0x80, 0x03, 0x00, 0x00, 0xFF, 0xFF, 0x5A,0x6B};//预设读取指令数组
+        int flagData = 0;//正负标志位
+        //预设置零指令数组
+        public Form1()
+        {
+            InitializeComponent();
+            String[] portnames = SerialPort.GetPortNames();//获取当前串口名
+            foreach (var item in portnames)
+            {
+                comboBox1.Items.Add(item);//combobox显示当前串口名
+            }
+            TextBox.CheckForIllegalCrossThreadCalls = false;// 禁用跨线程调用检查控件？？（正确地表述？）
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 开启串口
+        /// </summary>
+        private void OpenPort_Click(object sender, EventArgs e)
+        {
+            Opencom();
+            void Opencom() {
+                try
+                {
+                    serialPort1.BaudRate = 38400;//波特率
+                    serialPort1.DataBits = 8; //数据位
+                    serialPort1.PortName = comboBox1.Text;//串口名
+                    serialPort1.StopBits = System.IO.Ports.StopBits.Two;//停止位
+                    serialPort1.Parity = Parity.None;//无奇偶校验
+                    serialPort1.ReadTimeout = 1000;//超时时间
+                    serialPort1.ReceivedBytesThreshold = 1; //必须一定要加上这句话，意思是接收缓冲区当中如果有一个字节的话就出发接收函数，如果不加上这句话，那就有时候触发接收有时候都发了好多次了也没有触发接收，有时候延时现象等等，
+                    serialPort1.Open();//打开串口
+                    if (!serialPort1.IsOpen)
+                    {
+                        MessageBox.Show("端口打开失败", "error");
+                        return;
+                    }
+                    else
+                    {
+                        textBoxInfo.AppendText("端口" + comboBox1.Text + "打开成功\r\n");
+                    }
+                    serialPort1.DataReceived += SerialPort1_DataReceived;// 表示端口接收到了数据，具体作用是？
+                }
+                catch (Exception ex) {
+                    serialPort1.Dispose();//出错滚逼串口
+                    textBoxInfo.AppendText(ex.Message);//显示错误信息
+                }
+            }
+        }
+
+        /// <summary>
+        /// 串口接收数据并处理
+        /// </summary>
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //throw new NotImplementedException();//抛出异常 来检测这一功能你实现了么有（使用串口类时自动会生成的）
+            Thread.Sleep(75);//（毫秒）等待一定的时间，确保数据的完整性 int len
+            int len = serialPort1.BytesToRead;//串口收到的字节的长度
+            string receivedata = string.Empty;//接受内容的string格式
+            receData = 0;//测量数值变量清零
+            //receData = receData;//serial_test内的全局变量存放测微仪的数据
+            if (len != 0) {
+                byte[] buffer = new byte[len];//创建缓存区变量
+                serialPort1.Read(buffer, 0, len);//将串口输入数据写入缓存区
+                for (int i = 0; i < len; i++) {
+                    receivedata = Convert.ToString(Convert.ToInt32(buffer[i]), 16);//将数据转码成字符
+                    if (buffer[i] < 10) {
+                        receivedata = "0" + receivedata;//小于十的前位补零
+                    }
+                    
+                    switch (i)//截选标志位和数据位
+                    {
+                        case 3:
+                            if ( receivedata == "00")//判别正负标志位
+                            {
+                                flagData = 1;
+                            }
+                            else {
+                                flagData = -1;
+                            }
+                            break;
+                        case 4:
+                            if (receivedata != "00") {
+                                receData = Convert.ToSingle ( buffer[i]);//录入数据
+                                //Console.WriteLine(" {0}", buffer[i]);//检测收取到的byte数据
+                            }
+                            break;
+                        case 5:
+                            if (receivedata != "00")
+                            {
+                                receData = receData * 256 + Convert.ToSingle(buffer[i]);//录入数据
+                                //Console.WriteLine(" {0}", buffer[i]);//检测收取到的byte数据
+                            }
+                            break;
+                        case 6:
+                            if (receivedata != "00")
+                            {
+                                receData = receData * 256 + Convert.ToSingle(buffer[i]);//录入数据
+                                //Console.WriteLine(" {0} is the byte", buffer[i]);//检测收取到的byte数据
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                   
+                    //textBox2.AppendText(receivedata + " ");//有待商榷
+                }
+                //textBox2.AppendText("The receData\r\n");
+                //receData = receData * flagData / 1000; //接受数据流转化测量数据
+                Console.WriteLine("The receData is {0} \r\n", receData);
+            }
+            //Console.WriteLine("receivedata is " + receivedata + "\r\n");
+            
+        }
+
+        /// <summary>
+        /// 关闭串口
+        /// </summary>
+        private void ClostPort_Click(object sender, EventArgs e)
+        {
+            serialPort1.Dispose();
+        }
+
+        /// <summary>
+        /// 串口输出数据流（十六进制数 以空格分隔）
+        /// </summary>
+        private void SendContent_Click(object sender, EventArgs e)
+        {
+            string str1 = textBoxSend.Text;//读取文本框字符
+            string[] textSend = str1.Split(new char[] {' '});//以空格分割
+            Console.WriteLine("testSend2 is " + textSend + "\r\n");//调试输出字符
+            //Console.WriteLine("testSend2.length is " + textSend.Length + "\r\n");
+            byte[] dataSend = new byte[textSend.Length];
+            for (int i = 0; i < textSend.Length; i++) {
+                dataSend[i] =Convert.ToByte( Convert.ToInt32(textSend[i],16));
+                //Console.WriteLine("dataSend{0} is " + dataSend[i] + "\r\n",i);
+            }
+            serialPort1.Write(dataSend,0,dataSend.Length);
+            //Console.WriteLine("sending text is " + " " + "\r\n");//调试输出
+        }
+
+        /// <summary>
+        /// 清空文本框及初始化
+        /// </summary>
+        private void buttonClearText_Click(object sender, EventArgs e)
+        {
+            textBoxSend.Text = "";
+            textBox2.Text = "";
+            textBoxInfo.Text = "";
+            InitChart();// 初始化图表
+            String[] portnames = SerialPort.GetPortNames();//重新获取当前串口名
+            comboBox1.Items.Clear();
+            foreach (var item in portnames)
+            {
+                comboBox1.Items.Add(item);//combobox显示当前串口名
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 开启定时器并实时出图
+        /// </summary>
+        private void buttonMeasure_Click(object sender, EventArgs e)
+        {
+            this.timer1.Start();//开始绘图
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 关闭定时器
+        /// </summary>
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();//停止计时器绘图
+        }
+
+        /// <summary>
+        /// 定时器 控制串口发送数据流，并处理返回数据输出在图表中
+        /// </summary>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            serialPort1.Write(measureDataByte,0,measureDataByte.Length);//串口输出读取数据指令
+            //if (Math.Abs(receData) >4)//接收的数据大于0.0004mm才更新数据
+            //{
+                UpdataQueueValue();//数据队列更新
+            //}
+            this.chart1.Series[0].Points.Clear();//清空图表点
+            for (int i = 0; i < dataQueue.Count; i++) {
+                this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));//更新图表
+                //Console.WriteLine("queue Element is {0}\r\n", dataQueue.ElementAt(i));//监视队列中的数据输出
+            }
+        }
+
+        /// <summary>
+        /// 初始化图表
+        /// </summary>
+        private void InitChart()
+        {
+            //定义图表区域
+            this.chart1.ChartAreas.Clear();
+            ChartArea chartArea1 = new ChartArea("C1");
+            this.chart1.ChartAreas.Add(chartArea1);
+            //定义储存和显示点的容器
+            this.chart1.Series.Clear();
+            Series series1 = new Series("S1");
+            series1.ChartArea = "C1";
+            this.chart1.Series.Add(series1);
+            //设置图标显示样式
+            this.chart1.ChartAreas[0].AxisX.Interval = 1;
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            //设置标题
+            this.chart1.Titles.Clear();
+            this.chart1.Titles.Add("S01");
+            this.chart1.Titles[0].Text = "Serial Port";
+            this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
+            this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            //图表样式
+            this.chart1.Series[0].Color = Color.MidnightBlue;
+            this.chart1.Titles[0].Text = string.Format("测微仪 显示");
+            this.chart1.Series[0].ChartType = SeriesChartType.Line;
+            //claer
+            this.chart1.Series[0].Points.Clear();
+        }
+
+        /// <summary>
+        /// 更新队列中的值
+        /// </summary>
+        private void UpdataQueueValue() {
+            if (dataQueue.Count > 50) {
+                for (int i = 0; i < numDelete; i++) { //先出列
+                    dataQueue.Dequeue();
+                }
+            }
+            for (int i = 0; i < numDelete; i++) {//输入值
+                //if (receData > 100) {
+                    receData = receData * flagData / 10000;//receData = receData / 1000; //单位转化
+                //}
+                if (dataQueue.Count>49) { 
+                    if (receData != 0 && ( Math.Abs(receData)<0.0005 ) &&(Math.Abs( Convert.ToSingle( dataQueue.ElementAt(49))) > 0.0004) || (receData == 0 && (Math.Abs( Convert.ToSingle( dataQueue.ElementAt(49))))>0.001) || (receData>1000)) {
+                        receData = Convert.ToSingle( dataQueue.ElementAt(49));
+                    }
+                }
+                
+                dataQueue.Enqueue(receData);
+                Console.WriteLine("Queue receData is {0} \r\n", receData);//队列数据输出
+            }
+        }
+    }
+}
